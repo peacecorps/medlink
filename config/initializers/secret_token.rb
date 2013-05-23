c3bb4c3c7c000fe6945155f25672cb1633f5f197
James Dabbs <jamesdabbs@gmail.com>
2013-05-23 19:44:34 -0400

    Generated project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rhok::Application.config.secret_token = 'df579a21910c45e9ec0286752c4da4f1a607f2e2f8e5727878ff3c9d16977fabfa8348519897147c9e2ad3b83f6adcd06f055458239201a66fa12b759d77bf13'
