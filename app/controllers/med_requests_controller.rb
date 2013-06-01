395f37c9cab38806ffea9a391587fbd4b239a3b7
Clint Lee <clint@theflywheelgroup.com>
2013-06-01 13:12:59 -0400

    fix bug in med requests controller

--- app/controllers/med_requests_controller.rb
+++ app/controllers/med_requests_controller.rb
@@ -3,7 +3,7 @@ require 'twilio-ruby'
 class MedRequestsController < ApplicationController
 
 	def create
-		from = params[:from]
+		from   = params[:from]
 		body   = params[:body]
 		client = Twilio::REST::Client.new ENV['TWILIO_ACCOUNT_SID'], ENV['TWILIO_AUTH_TOKEN']
 		client.account.sms.messages.create(
