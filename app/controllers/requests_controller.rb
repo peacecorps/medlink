d23ae20ded8ebd995be18e94bcf3d6f045a0de78
Jonathan Howard <veniaveneficus@gmail.com>
2013-06-01 14:30:20 -0400

    Added scaffolded Requests controller

---
+++ app/controllers/requests_controller.rb
@@ -0,0 +1,19 @@
+class RequestsController < ApplicationController
+  def new
+  end
+
+  def create
+  end
+
+  def delete
+  end
+
+  def destroy
+  end
+
+  def edit
+  end
+
+  def update
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,18 @@
 require 'resque/server'
 
 Rhok::Application.routes.draw do
+  get "requests/new"
+
+  get "requests/create"
+
+  get "requests/delete"
+
+  get "requests/destroy"
+
+  get "requests/edit"
+
+  get "requests/update"
+
   devise_for :users
 
   resources :users
