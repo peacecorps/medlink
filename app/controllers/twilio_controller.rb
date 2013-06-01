4c369c2886431707b68d3148dfdb45da451b1471
James Dabbs <jamesdabbs@gmail.com>
2013-06-01 19:37:40 -0400

    Passing Order#from_test specs

--- app/controllers/twilio_controller.rb
+++ app/controllers/twilio_controller.rb
@@ -3,12 +3,11 @@ require 'twilio-ruby'
 class TwilioController < ApplicationController
 
 	def create
-    data = SMS.parse params
+    data  = SMS.parse params
     order = Order.create_from_text data
-
-    # The Order object and its associated Request may or may not be
-    # valid or persisted
     SMS.send_confirmation order
+  rescue => e
+    SMS.send_confirmation e.message
 	end
   
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -15,25 +15,36 @@ class Order < ActiveRecord::Base
   validates :user_id, presence: true
 
   def self.create_from_text data
-    user   = User.where(pcv_id: data[:pcvid]).first
-    hub    = PcHub.where(name: data[:loc]).first
-    supply = Supply.where(shortcode: data[:shortcode]).first
+    user   = User.where(pcv_id: data[:pcvid]).first || raise("Unrecognized PCVID")
+    hub    = PcHub.where(name: data[:loc]).first || raise("Unrecognized location")
+    supply = Supply.where(shortcode: data[:shortcode]).first || raise("Unrecognized shortcode")
 
-    order = new({
+    order = create!({
       user_id:   user.try(:id),
       pc_hub_id: hub.try(:id),
       phone:     data[:phone] || user.try(:phone),
       email:     data[:email] || user.try(:email)
     })
-
-    if order.save
-      order.requests.create({
+    
+    begin
+      order.requests.create!({
         supply_id: supply.try(:id),
         dose:      "#{data[:dosage_value]}#{data[:dosage_units]}",
         quantity:  data[:qty]
       })
+    rescue ActiveRecord::RecordNotValid => e
+      order.destroy
+      raise
     end
 
     order
+  end
+
+  def confirmation_message
+    if self.valid?
+      "Your request has been processed and action will be taken within 24 hours."
+    else
+      errors.each_full.join(" ")
+    end
   end
 end
