320ac0b8ed7ee89f153f27803d9b5fb917431ca9
James Dabbs <jamesdabbs@gmail.com>
2013-06-01 17:25:18 -0400

    Split factories

---
+++ spec/factories/supply.rb
@@ -0,0 +1,4 @@
+FactoryGirl.define do
+  factory :supply do
+  end
+end
--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,14 +1,6 @@
-require 'factory_girl'
-
 FactoryGirl.define do
   factory :user do
     email    'user@example.com'
     password 'password'
-  end
-
-  factory :supply do
-  end
-
-  factory :request do
   end
 end
