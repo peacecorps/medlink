84f28404f434ca381017cbe18448cc008f5be8f3
Clint Lee <clint@theflywheelgroup.com>
2013-06-01 13:33:46 -0400

    fix bug with twilio env

--- app/controllers/med_requests_controller.rb
+++ app/controllers/med_requests_controller.rb
@@ -5,7 +5,7 @@ class MedRequestsController < ApplicationController
 	def create
 		from   = params[:From]
 		body   = params[:Body]
-		client = Twilio::REST::Client.new ENV['TWILIO_ACCOUNT_SID'], ENV['TWILIO_AUTH_TOKEN']
+		client = Twilio::REST::Client.new ENV['TWILIO_ACCOUNT_SID'], ENV['TWILIO_AUTH']
 		client.account.sms.messages.create(
   			:from => '+17322301185',
   			:to => from,
