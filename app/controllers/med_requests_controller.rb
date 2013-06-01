75782455c779ee36a2bf0c860cee592de398dc5b
Clint Lee <clint@theflywheelgroup.com>
2013-06-01 13:06:59 -0400

    add twilio

--- app/controllers/med_requests_controller.rb
+++ app/controllers/med_requests_controller.rb
@@ -5,7 +5,7 @@ class MedRequestsController < ApplicationController
 	def create
 		from = params[:from]
 		body   = params[:body]
-		client = Twilio::REST::Client.new TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN
+		client = Twilio::REST::Client.new ENV['TWILIO_ACCOUNT_SID'], ENV['TWILIO_AUTH_TOKEN']
 		client.account.sms.messages.create(
   			:from => '+17322301185',
   			:to => from,
