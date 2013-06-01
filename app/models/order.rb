194d20c8a69dfb5ec8c5ef90f561b80b246f55a3
James Dabbs <jamesdabbs@gmail.com>
2013-06-01 17:29:48 -0400

    Update relations for order-related models

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,3 +1,6 @@
 class Order < ActiveRecord::Base
   attr_accessible :confirmed, :email, :extra, :fulfilled, :pc_hub_id, :phone, :user_id
+
+  belongs_to :user
+  has_many :requests
 end
--- app/models/request.rb
+++ app/models/request.rb
@@ -2,7 +2,7 @@ class Request < ActiveRecord::Base
   attr_accessible :dose, :location, :quantity, :state, :supply_id, :user_id
   attr_accessible :phone, :email
 
-  belongs_to :user
+  belongs_to :order
   belongs_to :supply
 
   HUMANIZED_ATTRIBUTES = {
