f3600ed255b8c6794edd20b15003af1f749b1a57
James Dabbs <jamesdabbs@gmail.com>
2013-06-01 17:22:31 -0400

    Add spec for Supply#unit

--- spec/models/supply_spec.rb
+++ spec/models/supply_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe Supply do
   subject { FactoryGirl.create :supply }
 
-  it 'can be created' do
-    expect( subject ).to be_a_kind_of Supply
-  end
+  it { should be_a_kind_of Supply }
+
+  it 'knows what unit options it accepts'
 end
