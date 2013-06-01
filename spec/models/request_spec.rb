742943017b4b7d8595dd59ae93111e6ea239a321
James Dabbs <jamesdabbs@gmail.com>
2013-06-01 14:29:03 -0400

    Stub Request specs

--- spec/models/request_spec.rb
+++ spec/models/request_spec.rb
@@ -6,4 +6,33 @@ describe Request do
   it 'can be created' do
     expect( subject ).to be_a_kind_of Request
   end
+
+  context 'received' do
+    it 'has not been confirmed' do
+      expect( subject ).not_to be_confirmed
+    end
+
+    it 'can send a confirmation'
+    it 'notifies if invalid'
+    it 'rejects duplicates'
+  end
+
+  context 'confirmed' do
+    before(:each) { subject.confirm! }
+
+    it 'has been confirmed' do
+      expect( subject ).to be_confirmed
+    end
+
+    it 'has not been replied to' do
+      expect( subject ).not_to be_confirmed
+    end
+
+    it 'can be completed'
+  end
+
+  context 'completed' do
+    it 'has instructions for pickup'
+    it 'can send those instructions'
+  end
 end
