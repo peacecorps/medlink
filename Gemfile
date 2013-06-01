64dc43049fad489c8e88f7c038ce3f663f280b48
Clint Lee <clint@theflywheelgroup.com>
2013-06-01 12:38:54 -0400

    setup twilio

--- Gemfile
+++ Gemfile
@@ -4,6 +4,7 @@ gem 'rails', '3.2.13'
 gem 'jquery-rails'
 gem 'haml-rails'
 gem 'twitter-bootstrap-rails'
+gem 'twilio-ruby'
 
 # Gems used only for assets and not required
 # in production environments by default.
---
+++ app/controllers/med_requests_controller.rb
@@ -0,0 +1,14 @@
+class MedRequests < ApplicationController
+
+	def create
+		from = params[:from]
+		body   = params[:body]
+		client = Twilio::REST::Client.new TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN
+		client.account.sms.messages.create(
+  			:from => '+17322301185',
+  			:to => from,
+  			:body => body
+		)
+	end
+
+end
