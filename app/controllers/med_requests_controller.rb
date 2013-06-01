34903470726c4397cc6ca78ea8215968b653f066
Clint Lee <clint@theflywheelgroup.com>
2013-06-01 13:22:34 -0400

    fix issue with getting twilio params

--- app/controllers/med_requests_controller.rb
+++ app/controllers/med_requests_controller.rb
@@ -3,8 +3,8 @@ require 'twilio-ruby'
 class MedRequestsController < ApplicationController
 
 	def create
-		from   = params[:from]
-		body   = params[:body]
+		from   = params[:From]
+		body   = params[:Body]
 		client = Twilio::REST::Client.new ENV['TWILIO_ACCOUNT_SID'], ENV['TWILIO_AUTH_TOKEN']
 		client.account.sms.messages.create(
   			:from => '+17322301185',
