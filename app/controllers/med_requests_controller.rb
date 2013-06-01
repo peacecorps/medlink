8513a88485a2ebcb126a95fa94b0c60869fb23c0
Clint Lee <clint@theflywheelgroup.com>
2013-06-01 14:08:19 -0400

    twilio

--- app/controllers/med_requests_controller.rb
+++ app/controllers/med_requests_controller.rb
@@ -3,8 +3,11 @@ require 'twilio-ruby'
 class MedRequestsController < ApplicationController
 
 	def create
+		# snag parameters from twilio
 		from   = params[:From]
 		body   = params[:Body]
+
+		# send SMS
 		client = Twilio::REST::Client.new ENV['TWILIO_ACCOUNT_SID'], ENV['TWILIO_AUTH']
 		client.account.sms.messages.create(
   			:from => '+17322301185',
