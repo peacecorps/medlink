428b82e15ecdadfe9ebe47f4bfcd7844d4f3611f
Jake Swanson <jakswa@gmail.com>
2013-06-01 16:13:31 -0400

    create action and associated specs

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -1,8 +1,17 @@
 class RequestsController < ApplicationController
-  def create
-  end
+  respond_to :json
 
-  def delete
+  def create
+    attributes = params.slice(
+      :dose, :location, :quantity, :state, :supply_id, :user_id,
+      :phone, :email
+    )
+    @request = Request.new(attributes)
+    if @request.save
+      render json: {success: true}
+    else
+      render json: {errors: @request.errors, success: false}
+    end
   end
 
   def destroy
