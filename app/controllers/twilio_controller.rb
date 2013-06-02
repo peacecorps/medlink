519ab70738aea3a0c9fa34dbe749233c60aa6e6d
Clint Lee <clint@theflywheelgroup.com>
2013-06-01 21:58:57 -0400

    add rescue and head response to twilio controller

--- app/controllers/twilio_controller.rb
+++ app/controllers/twilio_controller.rb
@@ -4,15 +4,18 @@ class TwilioController < ApplicationController
 
 	def create
 		# parse gives back and SMS object
-    	sms = SMS.parse params
+        begin
+        	sms = SMS.parse params
+            if sms.send_now?
+                sms.send
+            else
+                create_order sms
+            end
         rescue => e
             SMS.send_error params[:From], e.message
+        end
 
-    	if sms.send_now?
-    		sms.send
-    	else
-            create_order sms
-		end    
+        head :ok
 	end
 
     private
@@ -21,7 +24,7 @@ class TwilioController < ApplicationController
         order = Order.create_from_text sms
         SMS.send_from_order order
         
-        rescue => e
-            SMS.send_error sms.data[:phone], e.message
+    rescue => e
+        SMS.send_error sms.data[:phone], e.message
     end
 end
