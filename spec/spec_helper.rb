9b34c0526d088722df3c4c0a8473b49484c97e9d
James Dabbs <jamesdabbs@gmail.com>
2013-06-02 00:26:26 -0400

    Add simplecov for spec coverage

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,11 @@
-# This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV["RAILS_ENV"] ||= 'test'
+
+# Run specs under COVERAGE=true to generate coverage reports
+if ENV["COVERAGE"]
+  require 'simplecov'
+  SimpleCov.start 'rails'
+end
+
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 require 'rspec/autorun'
