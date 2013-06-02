35a912f350bfbc396e15670d53872e7763d7f32f
James Dabbs <jamesdabbs@gmail.com>
2013-06-02 01:43:56 -0400

    Sign up with country

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,8 +6,8 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me
-  # attr_accessible :title, :body
+  attr_accessible :email, :password, :password_confirmation, :remember_me,
+    :country_id
 
   belongs_to :country
   has_many :orders
