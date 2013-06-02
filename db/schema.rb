31a08edbf8ac9d5f20c2eb5b72c1d49287724bcf
Jonathan Howard <veniaveneficus@gmail.com>
2013-06-01 23:15:44 -0400

    Forgot to check in schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,19 +11,16 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130602020623) do
+ActiveRecord::Schema.define(:version => 20130602031221) do
 
   create_table "countries", :force => true do |t|
     t.string   "name"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.string   "code"
   end
 
-  create_table "countries", :force => true do |t|
-    t.string   "name"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
-  end
+  add_index "countries", ["code"], :name => "index_countries_on_code"
 
   create_table "orders", :force => true do |t|
     t.integer  "user_id"
