99059e7a179738e41d42ab6bf600ca10d2f3a4b1
Clint Lee <clint@theflywheelgroup.com>
2013-06-01 22:43:03 -0400

    controller errors

--- app/controllers/twilio_controller.rb
+++ app/controllers/twilio_controller.rb
@@ -5,15 +5,15 @@ class TwilioController < ApplicationController
 	def create
 		# parse gives back and SMS object
         begin
+            Rails.logger.info("Starting create method...")
         	sms = SMS.parse params
             if sms.send_now?
                 sms.send
             else
                 create_order sms
             end
-            head :ok
         rescue => e
-            puts "error in parse"
+            Rails.logger.info "error in parse"
             SMS.send_error params[:From], e.message
         end
         head :no_content
@@ -26,7 +26,7 @@ class TwilioController < ApplicationController
         SMS.send_from_order order
         
     rescue => e
-        puts "Error in create_order"
+        Rails.logger.info "Error in create_order"
         SMS.send_error sms.data[:phone], e.message
     end
 end
