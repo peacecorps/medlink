4ed4d713e3510e873abb1868677ef36c95515de3
Jonathan Howard <veniaveneficus@gmail.com>
2013-06-02 07:56:23 -0400

    Initial set-up for Mandrill SMTP server

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -50,6 +50,10 @@ Rhok::Application.configure do
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default :charset => "utf-8"
 
   # Enable threaded mode
   # config.threadsafe!
