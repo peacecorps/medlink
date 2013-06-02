1440acea607bf589bcfe2b3c67c834d19bc03b1b
Jonathan Howard <veniaveneficus@gmail.com>
2013-06-02 07:54:19 -0400

    Added `letter_openener` gem

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -37,4 +37,5 @@ Rhok::Application.configure do
 
   # For Devise
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.delivery_method = :letter_opener
 end
