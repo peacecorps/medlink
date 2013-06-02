ff1fd14625853d249cba2eca51f1ed859053788f
James Dabbs <jamesdabbs@gmail.com>
2013-06-02 01:50:39 -0400

    Add current user endpoint

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,20 @@
 class UsersController < ApplicationController
+  before_filter :authenticate_user!
+  respond_to :json
+
 	def create
 	end
+
 	def destroy
 	end
+
 	def update
 	end
+
 	def index
 	end
+
+  def current
+    respond_with current_user
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,11 @@ Rhok::Application.routes.draw do
   devise_for :users
 
   resources :orders
-  resources :users
+
+  resources :users, only: [:create, :destroy, :update, :index] do
+    collection { get :current }
+  end
+
   resources :supplies, only: [:index]
   resources :requests, only: [:create, :destroy, :update]
 
