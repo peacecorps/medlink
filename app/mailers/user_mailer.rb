9513852ad458f7cc5a55018aa1040a74570a5d16
Jonathan Howard <veniaveneficus@gmail.com>
2013-06-02 08:42:48 -0400

    Initial commit of UserMailer
    All features outlined in issue #3 have been stubbed out. Forgotten passsword
    is feature complete (though untested) from a devise perspective. Views still
    need to be written for the remaining two stubs.

---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,18 @@
+class UserMailer < ActionMailer::Base
+  default from: "no-reply@pcmedsupply.com"
+
+  def welcome_email(user)
+    @user = user
+    @url = "http://pcmedsupply.com/users/sign_in"
+    mail(:to => user.email, :subject => "Thanks for signing up for pcmedsupply.org")
+  end
+
+  def forgotten_password_email(params)
+    User.send_reset_password_instructions params[:email]
+  end
+
+  def fufillment_email(order)
+    @order = order
+    mail(:to => order.email, :subject => "Your Order Has Been Fufilled")
+  end
+end
