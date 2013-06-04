9de3ae56f73d232bd87e7f1f2c8c2a13f32d4390
James Dabbs <jamesdabbs@gmail.com>
2013-06-03 23:46:50 -0400

    Improve Twilio controller specs

--- spec/controllers/twilio_controller_spec.rb
+++ spec/controllers/twilio_controller_spec.rb
@@ -1,12 +1,20 @@
 require 'spec_helper'
 
 describe TwilioController do
-  let(:current_user) { FactoryGirl.create(:user) }
-  before { sign_in current_user }
+  let(:current_user) { FactoryGirl.create(:user, pcv_id: '123456') }
+
+  before(:each) do
+    FactoryGirl.create :supply, shortcode: 'ASDF'
+    sign_in current_user
+  end
 
   describe "POST 'receive'" do
     it 'adds orders on parseable texts'
-    it 'sends a confirmation message after adding an order'
+
+    it 'sends a confirmation message after adding an order' do
+      SMS.should_receive(:send_raw).and_return(true)
+      post 'receive', From: '+15555555555', Body: '123456, ASDF, 30mg, 50, Somewhere'
+    end
 
     it 'sends an error message on unparseable texts' do
       SMS.should_receive(:send_raw).
