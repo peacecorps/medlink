07eb3bdffe8829f9e58f60254577ae5d94566bf6
James Dabbs <jamesdabbs@gmail.com>
2013-06-04 20:55:45 -0400

    Remove unused mailer method

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,12 +1,6 @@
 class UserMailer < ActionMailer::Base
   default from: "no-reply@pcmedsupply.com"
 
-  def welcome id
-    @user = User.find id
-    @url = "http://pcmedsupply.com/users/sign_in"
-    mail to: @user.email, subject: "Thanks for signing up for pcmedsupply.org"
-  end
-
   def forgotten_password id
     User.find(id).send_reset_password_instructions async: false
   end
