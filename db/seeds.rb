a014711b35356cafba36aa6873df52a32747aeb8
Al Snow <jasnow@hotmail.com>
2013-06-09 11:19:38 -0400

    Moved populate script into db/seeds.rb so it is run during 'rake db:setup'

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+require 'csv'
+
+CSV.read(Rails.root+"lib/tasks/supply.csv").each { |a|
+  Supply.new(:shortcode => a[1], :name => a[0]).save
+}
+
+CSV.read(Rails.root+"lib/tasks/country.csv", { :col_sep => ";" }).each { |a|
+  Country.new(:code => a[1], :name => a[0]).save
+}
+
+CSV.read(Rails.root+"lib/tasks/user.csv").each do |a|
+  role = a[1] == 'Dabbs' ? 'admin' : nil
+  User.new(:first_name => a[0], :last_name => a[1], :country_id => 1, :email => a[2], :pcv_id => a[3], :role => role).save
+end
