2c35b36e7929b10ea9bfc009785ea4a9cef16abb
Gordon Macie <gmacie@gmail.com>
2013-08-16 07:58:16 -0400

    initial report controller

---
+++ app/controllers/report_controller.rb
@@ -0,0 +1,19 @@
+class ReportController < ApplicationController
+  def request_history
+  end
+
+  def fulfillment_history
+  end
+
+  def recent_adds
+  end
+
+  def recent_edits
+  end
+
+  def pcmo_response_times
+  end
+
+  def supply_history
+  end
+end
---
+++ spec/views/report/recent_edits.html.haml_spec.rb
@@ -0,0 +1,5 @@
+require 'spec_helper'
+
+describe "report/recent_edits.html.haml" do
+  pending "add some examples to (or delete) #{__FILE__}"
+end
