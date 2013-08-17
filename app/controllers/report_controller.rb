3819b0c83d4c3a441106bc651b7b31782f5c08a1
Gordon Macie <gmacie@gmail.com>
2013-08-17 12:04:11 -0400

    recent user edits report

--- app/controllers/report_controller.rb
+++ app/controllers/report_controller.rb
@@ -4,18 +4,23 @@ class ReportController < ApplicationController
   end
 
   def fulfillment_history
+    @orders = current_user.accessible_orders
   end
 
   def recent_adds
-    @user
+    @users = User.all
+    # User.where("updated_at >= :start_date AND updated_at <= :end_date",
+                #{start_date: params[:start_date], end_date: params[:end_date]})
   end
 
   def recent_edits
+    @users = User.all
   end
 
   def pcmo_response_times
   end
 
   def supply_history
+    @supplies = Supply.all
   end
 end
