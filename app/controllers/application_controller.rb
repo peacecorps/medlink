d826c7dbf11ab164741597cd08a856d6c22ac36b
James Dabbs <jamesdabbs@gmail.com>
2013-09-15 14:02:43 -0400

    Remove unused ip-checking logic
    We won't be using it for the pilot, and maybe ever. Let's build it
    _after_ we have requirements.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,12 +9,8 @@
 #   You have been successfully signed out.
 # session new
 #   Invalid email or password.
-require 'ipaddr'
-
 class ApplicationController < ActionController::Base
   protect_from_forgery
-
-  # before_action :check_ip_for_admins
 
   def root
     authenticate_user!
@@ -27,24 +23,6 @@ class ApplicationController < ActionController::Base
   end
 
   private # ----------
-
-  def ip_allowed?
-    request_ip  = IPAddr.new request.remote_ip
-    Rails.configuration.allowed_ips.any? do |addr|
-      IPAddr.new(addr).include? request_ip
-    end
-  end
-
-  def check_ip_for_admins
-    if current_user && current_user.admin?
-      if !ip_allowed?
-        sign_out current_user
-        redirect_to new_user_session_path,
-          notice: 'Admin users may only login from approved ip addresses'
-      end
-    end
-  end
-
 
   # Redirects to the login path to allow the flash messages to display for sign_out.
   def after_sign_out_path_for(resource_or_scope)
