d34a16fa93fd93c8afa4e39699e7a5aabf8edc92
Al Snow <jasnow@hotmail.com>
2013-09-19 12:27:15 -0400

    Verified 'add devise msgs' FIXME

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,3 @@
-# FIXME: add devise messages
-# registration edit
-#   Successfully updated your profile.
-#   There was a problem with your profile.
-# registration create
-#   Welcome! Please sign in with your new account.
-#   There was a problem with your information.
-# session destroy
-#   You have been successfully signed out.
-# session new
-#   Invalid email or password.
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
