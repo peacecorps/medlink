763c128a010f58077782e64c075ac01a10cf1fbb
Al Snow <jasnow@hotmail.com>
2013-10-13 14:00:43 -0400

    Added R1 to R4 design doc tags

--- app/models/delivery_method.rb
+++ app/models/delivery_method.rb
@@ -15,13 +15,17 @@ class DeliveryMethod
     include Enumerable
 
     def each
+      # R3
       yield new :delivery, 'Your request is estimated to arrive at your ' +
         'location on this date [enter date here].'
+      # R1
       yield new :pickup, 'Please pick up your request at this ' +
         '[enter location here] by this [enter date]'
+      # R2
       yield new :purchase, 'We do not have the requested item in stock. ' +
         'Please purchase elsewhere and allow us to reimburse you.',
         'Purchase & Reimburse'
+      # R4
       yield new :special, 'Please contact me at this [phone number] ' +
         'concerning your request.', 'Special Instructions'
     end
