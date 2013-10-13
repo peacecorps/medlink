4300bbb2cdfcb4e00a014c45a1c4725779324a74
Al Snow <jasnow@hotmail.com>
2013-10-13 18:58:23 -0400

    #146: Added validation

--- app/models/response.rb
+++ app/models/response.rb
@@ -2,7 +2,9 @@ class Response < ActiveRecord::Base
   belongs_to :order
 
   validates_presence_of :delivery_method, message: "is missing"
-  validates :instructions, length: { maximum: 160 }
+  validates :instructions, length: { maximum: 160 },
+    format: { :with => /\A[a-zA-Z0-9\.\/ ]*\z/,
+    :message => "- Please replace the [placeholders] with values." }
 
   def send!
     to = order.phone || order.user.phone
