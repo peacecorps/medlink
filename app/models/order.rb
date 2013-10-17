62dd0391bd0908aea6c96d0f9fc5ef4f53244195
Al Snow <jasnow@hotmail.com>
2013-10-17 11:19:33 -0400

    #123: Added other 2 of 3 response-related columns

--- app/models/order.rb
+++ app/models/order.rb
@@ -23,6 +23,26 @@ class Order < ActiveRecord::Base
   scope :pending,  -> { unresponded.where(["orders.created_at >= ?",
     3.business_days.ago]) }
 
+  def response_time
+    #FIXME: Absolute delta response time or remove weekends
+    response && ((response.created_at - created_at) / (60 * 60 * 24)).round(1)
+  end
+
+  def how_past_due
+    # How many days over 3 are we past due?
+    if response
+      past_due = (
+        (3.business_days.after(created_at) -
+          response.created_at) / (60 * 60 * 24)
+      ).round(1)
+      if (past_due < 3)
+        0
+      else
+        (past_due - 3).round(1)
+      end
+    end
+  end
+
   def responded?
     response.present?
   end
