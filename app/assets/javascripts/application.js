390a671572bff080618df0b5bfdf44c8b6913707
Chenheli Hua <huachenheli@outlook.com>
2013-11-04 14:32:28 -0500

    Made navbar responsive so that menu items are moved into gear on mobile devices. Removed vertical bar on navbar.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,3 +17,27 @@
 //= require bootstrap-datepicker
 //= require daterangepicker
 
+$(document).ready(function(){
+
+    // Copy admin menu into gear
+    var list = $("ul[class=nav]").html();
+    $("ul[class=dropdown-menu]").prepend(list);
+
+    // Move admin menu into the gear when width is less than 980
+    function checkWidth(){
+	if($(window).width()<980){
+	    // Display menu only in gear
+	    $("ul[class=dropdown-menu] li:lt(5)").css("display","inline");
+	    $("ul[class=nav]").css("display","none");
+	}
+	else{
+	    // Display menu, and remove it from gear
+	    $("ul[class=dropdown-menu] li:lt(4)").css("display","none");
+	    $("ul[class=nav]").css("display","block");
+	}
+    }
+
+    // Run checkWidth on load and when resizing
+    checkWidth();
+    $(window).resize(checkWidth);
+});
