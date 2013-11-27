a2ebc9f8f45778e91205ead5f9abdf645e9c3f90
Al Snow <jasnow@hotmail.com>
2013-11-27 16:26:55 -0500

    #247/again: worked on invalid pw reset token; reverted previous commit; removed _ at end of help file

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -51,7 +51,7 @@ Medlink::Application.configure do
   # Disable delivery errors, bad email addresses will be ignored
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.raise_delivery_errors = false
   config.action_mailer.default :charset => "utf-8"
   config.action_mailer.default_url_options = { host: 'pcmedlink.org' }
 
