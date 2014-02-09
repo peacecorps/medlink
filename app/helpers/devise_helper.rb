3a784d298fbe75b61fd1b3305d67cba0356464ab
James Dabbs <jamesdabbs@gmail.com>
2014-02-09 15:01:57 -0500

    Extract devise translation logic to helper

--- app/helpers/devise_helper.rb
+++ app/helpers/devise_helper.rb
@@ -16,4 +16,17 @@ module DeviseHelper
 
     html.html_safe
   end
+
+  def translate_flash_class klass
+    case klass.to_sym
+    when :error
+      :danger
+    when :notice
+      :success
+    when :alert
+      :info
+    else
+      klass.to_sym
+    end
+  end
 end
