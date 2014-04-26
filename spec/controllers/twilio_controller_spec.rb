45df14960f1fd64d5b59ada4aea78a8ad74a2a31
James Dabbs <jamesdabbs@gmail.com>
2014-04-26 13:01:36 -0400

    Start partial duplicate request spec
    Stop upon realizing that I have no idea what the spec should be.

--- spec/controllers/twilio_controller_spec.rb
+++ spec/controllers/twilio_controller_spec.rb
@@ -1,6 +1,12 @@
 require 'spec_helper'
 
 describe TwilioController do
+  def request user, body
+    post :receive, AccountSid: ENV.fetch("TWILIO_ACCOUNT_SID"),
+      From: user.primary_phone.number,
+      Body: body
+  end
+
   before :each do
     %w(Sup wit dat).each { |n| create :supply, name: n, shortcode: n }
     @user = create :user, pcv_id: 'asdf'
@@ -9,10 +15,7 @@ describe TwilioController do
 
   it "can create multiple orders from an incoming text" do
     body = "Sup wit - Please"
-
-    post :receive, AccountSid: ENV.fetch("TWILIO_ACCOUNT_SID"),
-      From: @user.primary_phone.number,
-      Body: body
+    request @user, body
 
     expect( SMS.incoming.last.text ).to eq body
     expect( @user.orders.map { |o| o.supply.name }.sort ).to eq %w(Sup wit)
@@ -21,10 +24,7 @@ describe TwilioController do
 
   it "responds with error messages when something is wrong" do
     body = "Bro - do you even liftM?"
-
-    post :receive, AccountSid: ENV.fetch("TWILIO_ACCOUNT_SID"),
-      From: @user.primary_phone.number,
-      Body: body
+    request @user, body
 
     expect( SMS.incoming.last.text ).to eq body
     expect( Order.count ).to be 0
@@ -32,14 +32,17 @@ describe TwilioController do
   end
 
   it "rejects duplicate messages" do
-    2.times do
-      post :receive, AccountSid: ENV.fetch("TWILIO_ACCOUNT_SID"),
-        From: @user.primary_phone.number,
-        Body: "Sup"
-    end
+    2.times { request @user, "Sup" }
 
     expect( Order.count ).to eq 1
     expect( SMS.outgoing.last.text ).to match /already received/
+  end
+
+  it "allows messages with some supply overlap" do
+    request @user, "Sup wit - first request"
+    request @user, "Wit dat - second request"
+
+    pending "Figure out what should happen with duplicate orders"
   end
 
   it "verifies that messages came from Twilio" do
