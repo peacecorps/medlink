407a0d90ef95df98f86a70114baf7353a43f6361
David Askew <davidaskew@gmail.com>
2014-09-06 15:13:59 -0400

    Added migration to populate country_supplies for all items/countries

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140906182957) do
+ActiveRecord::Schema.define(version: 20140906190416) do
 
   create_table "countries", force: true do |t|
     t.string "name"
