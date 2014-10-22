c11e599fbd99b88aacd3fdcd4a1324c56a3722c2
James Dabbs <jamesdabbs@gmail.com>
2014-10-22 08:26:20 -0400

    Disable PCV ID check for PCMOs. Fixes #368
    since they don't have a PCV ID.

--- app/controllers/passwords_controller.rb
+++ app/controllers/passwords_controller.rb
@@ -1,7 +1,7 @@
 class PasswordsController < Devise::PasswordsController
   def create
     user = User.find_by_email(params[:user][:email])
-    if !user || user.pcv_id != params[:user][:pcv_id]
+    if !user || (user.pcv? && user.pcv_id != params[:user][:pcv_id])
       flash[:error] = I18n.t! "flash.auth.password_reset_invalid"
       redirect_to new_user_password_path and return
     end
