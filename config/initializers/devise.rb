10fb153e4b056946bef23619a6f5f7876d835c1c
James Dabbs <jamesdabbs@gmail.com>
2014-12-17 11:54:36 -0500

    Extend account confirmation window. Fixes #384

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -102,7 +102,7 @@ Devise.setup do |config|
   # their account can't be confirmed with the token any more.
   # Default is nil, meaning there is no restriction on how long a user can take
   # before confirming their account.
-  config.confirm_within = 30.days
+  config.confirm_within = 1.year
 
   # If true, requires any email changes to be confirmed (exactly the same way as
   # initial account confirmation) to be applied. Requires additional unconfirmed_email
