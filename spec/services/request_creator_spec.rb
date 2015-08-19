149ea8d634fc71cf7d0d52cc5dccdbbf872bb977
James Dabbs <jamesdabbs@gmail.com>
2015-08-19 12:41:15 -0400

    Re-implement pending spec.

--- spec/services/request_creator_spec.rb
+++ spec/services/request_creator_spec.rb
@@ -1,14 +1,23 @@
+require "spec_helper"
+
 describe RequestCreator do
-  it 'can mark duplicate orders for the same supply'
-  # FIXME: this was the old user-object based test
-    #pcv    = create :pcv
-    #supply = create :supply
+  it 'can mark duplicate orders for the same supply' do
+    pcv    = create :pcv
+    supply = create :supply
 
-    #orders = 3.times.map { create :order, user: pcv, supply: supply }
-    #expect( orders.none? &:duplicated? ).to eq true
+    orders = 2.times.map { create :order, user: pcv, supply: supply }
+    expect( orders.none? &:duplicated? ).to eq true
 
-    #pcv.mark_updated_orders
-    #orders.map &:reload
-    #expect( orders.first(2).all? &:duplicated? ).to eq true
-    #expect( orders.last.duplicated? ).to be false
+    rc = RequestCreator.new(pcv, request: {
+      text:              "Updating old request",
+      user_id:           pcv.id,
+      orders_attributes: { "1" => { supply_id: supply.id } }
+    })
+
+    expect( rc.save ).to eq true
+
+    orders.map &:reload
+    expect( orders.all? &:duplicated? ).to eq true
+    expect( supply.orders.order(created_at: :desc).first.duplicated? ).to be false
+  end
 end
