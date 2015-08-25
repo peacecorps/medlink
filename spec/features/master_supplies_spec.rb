617a1d3867ea89db2fe194941e5a0bc2c225b0bd
Patrick Nielsen <ptnielsen55@gmail.com>
2015-08-25 14:54:57 -0400

    Working Amend Master Supply List Tests

--- spec/features/master_supplies_spec.rb
+++ spec/features/master_supplies_spec.rb
@@ -20,7 +20,7 @@ describe "Master Supply List" do
     visit supplies_path
 
     expect( alert.text ).to eq "You are not authorized to view that page"
-    expect(current_path).to eq manage_orders_path
+    expect( current_path ).to eq manage_orders_path
   end
 
   it "prevents pcv's from viewing master supply list" do
@@ -29,7 +29,57 @@ describe "Master Supply List" do
     visit supplies_path
 
     expect( alert.text ).to eq "You are not authorized to view that page"
-    expect(current_path).to eq new_request_path
+    expect( current_path ).to eq new_request_path
   end
 
+  it "allows admins to toggle orderability" do
+    admin = create :admin
+    login admin
+    visit supplies_path
+
+    save_and_open_page
+  end
+
+  describe "amend list" do
+    before :each do
+      admin = create :admin
+      login admin
+      visit new_supply_path
+    end
+
+    it "allows admins to add new items" do
+      within ".new_supply" do
+        fill_in "Name", with: "Test Supply"
+        fill_in "Shortcode", with: "TEST"
+        click_on "Save"
+      end
+
+      expect( Supply.last.name ).to eq "Test Supply"
+      expect( Supply.count ).to eq 2
+    end
+
+    it "does not save items without name" do
+      within "#new_supply" do
+        fill_in "Name", with: "Test Supply"
+        click_on "Save"
+      end
+
+      expect( Supply.last.name ).not_to eq "Test Supply"
+      expect( Supply.count ).to eq 1
+      expect( page.find("#new_supply") ).to have_selector("div", ".form-group has error")
+      expect( page.text ).to include "can't be blank"
+    end
+
+    it "does not save items without shortcode" do
+      within "#new_supply" do
+        fill_in "Shortcode", with: "TEST"
+        click_on "Save"
+      end
+
+      expect( Supply.last.shortcode ).not_to eq "TEST"
+      expect( Supply.count ).to eq 1
+      expect( page.find("#new_supply") ).to have_selector("div", ".form-group has error")
+      expect( page.text ).to include "can't be blank"
+    end
+  end
 end
