306650b79868e8c8b10c9f44563fc64387143c38
James Dabbs <jamesdabbs@gmail.com>
2015-12-05 16:32:34 -0500

    Set Sidekiq limits for Redis connection pool

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
 web: bundle exec puma -C config/puma.rb
-worker: bundle exec sidekiq -q default -q mailers
+worker: bundle exec sidekiq -q default -q mailers -c 6
--- app/controllers/admin/pages_controller.rb
+++ app/controllers/admin/pages_controller.rb
@@ -1,5 +1,6 @@
-class Admin::PagesController < AdminController
+class Admin::PagesController < ApplicationController
   def dashboard
+    authorize :admin, :manage?
     @upcoming_announcements = Announcement.includes(:country).next 5
     @recent_messages        = SMS.incoming.newest 5
   end
