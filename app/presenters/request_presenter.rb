b82c7ecea9f34d913626f5bc6a5f27e136d5d60c
James Dabbs <jamesdabbs@gmail.com>
2016-01-11 12:57:58 -0500

    Don't show HTML for replacement package link. Fixes #457

--- app/presenters/request_presenter.rb
+++ app/presenters/request_presenter.rb
@@ -9,7 +9,7 @@ class RequestPresenter < ApplicationPresenter
     if model.reorder_of_id.present?
       label = "lost package sent #{h.time_ago_in_words model.reorder_of.created_at} ago"
       path  = h.response_path model.reorder_of_id
-      "Replacement of #{h.link_to label, path}"
+      "Replacement of #{h.link_to label, path}".html_safe
     else
       model.text
     end
