12b74840cdec88786a1f903bb2e42f57593d9229
James Dabbs <jamesdabbs@gmail.com>
2016-01-29 19:15:18 -0500

    Fix intermittent test failure caused by random time zones

--- app/presenters/message_presenter.rb
+++ app/presenters/message_presenter.rb
@@ -2,7 +2,8 @@ class MessagePresenter < ApplicationPresenter
   delegate :text
 
   def created_at
-    "#{short_date model.created_at} @#{model.created_at.strftime '%H:%M'}"
+    dt = model.created_at.in_time_zone model.user.time_zone
+    "#{short_date dt, model.user.time_zone} @#{dt.strftime '%H:%M'}"
   end
 
   def user_link
--- spec/presenters/message_presenter_spec.rb
+++ spec/presenters/message_presenter_spec.rb
@@ -1,14 +1,15 @@
 require "rails_helper"
 
 RSpec.describe MessagePresenter do
-  Given(:message) { FactoryGirl.build :sms, created_at: DateTime.new(1990, 06, 12, 12, 34) }
+  Given(:created) { DateTime.new(1990, 06, 12, 12, 34).utc      }
+  Given(:message) { FactoryGirl.build :sms, created_at: created }
+  Given(:tz)      { message.user.time_zone                      }
 
   When(:result) { MessagePresenter.new message }
 
-  # TODO - this should expect and use a time zone
-  Then { result.created_at =~ /June 12, 1990 @..:.4/                    }
-  And  { result.user_link.include? "/users/#{message.user_id}/timeline" }
-  And  { result.email.include? "mailto:#{message.user.email}"           }
-  And  { result.number.include? "tel:#{message.phone.condensed}"        }
-  And  { result.country == message.user.country.name                    }
+  Then { result.created_at == created.in_time_zone(tz).strftime("%B %d, %Y @%H:%M") }
+  And  { result.user_link.include? "/users/#{message.user_id}/timeline"             }
+  And  { result.email.include? "mailto:#{message.user.email}"                       }
+  And  { result.number.include? "tel:#{message.phone.condensed}"                    }
+  And  { result.country == message.user.country.name                                }
 end
